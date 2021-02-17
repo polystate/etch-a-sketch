dda3cda06c9dbe13623353a81ee8cb87504f4f4c
polystate <dockadams333@gmail.com>
2021-02-18 00:29:21 +0700

    Added color schemes

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Etch-a-Sketch Project, a live sketch tool on the browser that allows you to pain
 
 Update 2/17/2021
 
-Created a fully customizable grid using CSS Grid and Javascript so that user can flexibly input both dimensions and size.
+Created a fully customizable grid using CSS Grid and Javascript so that user can flexibly input both dimensions and size. Dimensions and Grid Size customizable.
+
+Update 2/18/2021
+
+User can now paint over grid. Added different color schemes for color customization. Next I will allow user to customize whether he wants grid lines or not to get the pure sketch experience. Other ideas would be to add an eraser, or a right click to turn paint on and off to give more flexibility. Right now user only has clear entire grid option.
--- sketch.js
+++ sketch.js
@@ -1,6 +1,24 @@
 //Initialize Default Grid On Page Loard, Dimensions/Grid Size
 
-createGrid(8,320)
+createGrid(64,500)
+
+//Color Schemes
+
+const greyscale = function(){
+    randNum = Math.floor(Math.random() * 255);
+    console.log(randNum)
+    return `rgb(${randNum},${randNum},${randNum})`
+}
+const random = function(){
+    randNum1 = Math.floor(Math.random() * 255);
+    randNum2 = Math.floor(Math.random() * 255);
+    randNum3 = Math.floor(Math.random() * 255);
+    return `rgb(${randNum1},${randNum2},${randNum3})`
+}
+const rainbow = ["red","orange","yellow","green","blue","indigo","violet"];
+const newspaper = ["black","grey","lightgrey","silver","darkgrey","dimgrey","slategrey","gainsboro","whitesmoke"]
+const autumn = ["orange","brown","#181818","gold","#D9b382","darkorange","#B5651D","#B73239"];
+const test = ["#36454f"];
 
 //Functions
 
@@ -17,19 +35,41 @@ for(let i = 0; i < dimensions*dimensions; i++){
     grid_item = document.createElement("div");
     grid_item.setAttribute("class","grid-item");
     grid_item.setAttribute("id",`grid-item${i}`);
+    grid_item.addEventListener("mouseover",function(){
+        // console.log(`I am hovering over grid # ${i}`)
+        grid_square = document.getElementById(`grid-item${i}`)
+        grid_square.style = `background-color: ${randomColor(submitScheme())};`
+        
+    })
     grid.appendChild(grid_item);
 }
 }
-
-
 
 function submitCustomGrid(){
     areainput = document.querySelector("input");
     sizeinput = document.querySelectorAll("input")[1];
     dimensions = Number(areainput.value);
     size =  Number(sizeinput.value);
-    if(dimensions < 2 || dimensions > 24 || size < 120 || size > 640) return;
+    if(dimensions < 2 || dimensions > 64 || size < 320 || size > 640) return;
     grid.remove();
     createGrid(dimensions,size);
 }
 
+
+function clearGrid(){
+    gridsquares = document.getElementsByClassName("grid-item");
+    for(let i = 0; i < gridsquares.length; i++){
+        gridsquares[i].style = "background-color: white;"
+    }
+}
+
+function randomColor(arr){
+    if(typeof(arr) === "string") return arr;
+    randNum = Math.floor(Math.random() * arr.length);
+    return arr[randNum];
+}
+
+function submitScheme(){
+    optionSelect = document.getElementById("colors").value
+    return eval(optionSelect);
+}
