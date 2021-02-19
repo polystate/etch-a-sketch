ddb46b3a1032228a6c3ff60bfbc70c2efd9e3a7c
polystate <dockadams333@gmail.com>
2021-02-19 19:38:53 +0700

    Can now change canvas BG

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Introduction
 
-Etch-a-Sketch Project, a live sketch tool on the browser that allows you to paint with various colors, sizes, and grid dimensions. While programming this project, I hope to learn how to master CSS Grid and unlock the potential of the ::hover psuedoelement.
+Etch-a-Sketch Project, a live sketch tool on the browser that allows you to paint with various colors, sizes, and grid dimensions. While programming this project, I hope to learn how to master CSS Grid and unlock the potential of the ::hover psuedoelement. (Edit 2/19/21: What I Learned - We don't use the hover pseudoelement at all for the grid sketch. Instead we are mainly just using javascript event listeners, mainly 'mouseover.')
 
 Update 2/17/2021
 
@@ -13,3 +13,7 @@ User can now paint over grid. Added different color schemes for color customizat
 Update 2/19/2021
 
 User can now switch to eraser mode or deactivate drawing mode, allowing more freedom in sketching. Instructions also added and icons added for both draw and erase mode.
+
+Update 2/19/201 7:34 PM
+
+User can now change background of canvas. Clear button and change background merged into single button. Still have to fix bug where if we change background eraser will start drawing in that color. Also need to spruce up the buttons and inputs to make them look more interesting.
--- sketch.js
+++ sketch.js
@@ -2,10 +2,11 @@
 
 createGrid(64,500)
 
-//Drawing and Erasing Booleans
+//Drawing/Erasing Booleans and Background Color
 
 let isDrawing = true;
 let isErasing = false;
+// let bgColor = "white";
 
 //Color Schemes
 
@@ -29,7 +30,6 @@ const volcano = function(){
     return `rgb(${randNum},25,25)`
 }
 const rainbow = ["red","orange","yellow","green","blue","indigo","violet"];
-// const rainbow = ["#FF0000","#FF7F00","#FFFF00","#00FF00","#0000FF","#4B0082","#9400D3"];
 const newspaper = ["black","grey","lightgrey","silver","darkgrey","dimgrey","slategrey","gainsboro","whitesmoke"];
 const autumn = ["orange","brown","#181818","gold","#D9b382","darkorange","#B5651D","#B73239"];
 const christmas = ["#BB2528","#146B3A","#165B33","#EA4630","#F8B229"];
@@ -45,13 +45,14 @@ grid_container = document.createElement("div");
 grid_container.setAttribute("id","grid_container");
 grid_wrapper.appendChild(grid_container);
 grid = document.getElementById("grid_container");
-grid.style = `grid-template-columns: repeat(${dimensions}, ${pixels}px); grid-template-rows: repeat(${dimensions}, ${pixels}px); `
+grid.style = `grid-template-columns: repeat(${dimensions}, ${pixels}px); grid-template-rows: repeat(${dimensions}, ${pixels}px); background: ${currentBackground()}`
 
 for(let i = 0; i < dimensions*dimensions; i++){
     grid_item = document.createElement("div");
     grid_item.setAttribute("class","grid-item");
     grid_item.setAttribute("id",`grid-item${i}`);
     grid_item.addEventListener("contextmenu", e => e.preventDefault());
+    
     grid_item.addEventListener("mousedown", function(e){
         buttonClicked = logMouseButton(e);
         if(buttonClicked === "left"){
@@ -66,15 +67,26 @@ for(let i = 0; i < dimensions*dimensions; i++){
             console.log("isErasing is " + isErasing);
         } 
     })
+    // pageBody = document.querySelector("body");
+    // pageBody.addEventListener("mouseover",function(){
+    //     grid.style.background = currentBackground();
+    // })
     grid_item.addEventListener("mouseover",function(){
         grid_square = document.getElementById(`grid-item${i}`)
         if(isDrawing){
         grid_square.style = `background-color: ${randomColor(submitScheme())};`
         } else if(isErasing){
-        grid_square.style = `background-color: white;`
+        grid_square.style = `background-color: ${currentBackground()};`
         }
     })
     grid.appendChild(grid_item);
+    
+    // optionBG = document.querySelectorAll("option");
+    
+    // optionBG.addEventListener('click',function(){
+    //     grid.style.background = `${currentBackground()};`
+    // })
+    
 }
 }
 
@@ -92,7 +104,7 @@ function submitCustomGrid(){
 function clearGrid(){
     gridsquares = document.getElementsByClassName("grid-item");
     for(let i = 0; i < gridsquares.length; i++){
-        gridsquares[i].style = "background-color: white;"
+        gridsquares[i].style = `background-color: ${currentBackground()};`
     }
 }
 
@@ -121,4 +133,8 @@ function logMouseButton(e) {
       }
     }
   }
+
+  function currentBackground(){
+      return document.getElementById("bgcolor").value;
+}
 
